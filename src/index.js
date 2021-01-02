17be4c97ff52626d57cd8e34d90bfcf771ff433c
Samarth <samarth@pop-os.localdomain>
2021-01-02 22:01:45 +0530

    1 month cookie duration

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ app.use(
   cookieSession({
     name: "session",
     keys: [process.env.COOKIE_KEY],
-    maxAge: 30 * 24 * 60 * 60 * 1000,
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 1 month cookie expiration date
   })
 );
 
